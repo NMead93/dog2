3a8d136a24a04427ff4cb416afcec5c366df3e0c
nicholas mead and said ahmed <nicholas@>
2017-01-10 11:29:14 -0800

    add multiple buttons, add css styling & jQuery functionality to said buttons.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,27 @@
 $(document).ready(function() {
-  $("#green").click(function() {
-    $("body").toggleClass("green-background");
-
+  $(".green").click(function() {
+    $("body").addClass("green-background");
+    $(this).hide();
+    $(".lightgreen").show();
   });
 
-  $("#blue").click(function() {
-    $("body").toggleClass("lightgreen-background");
+  $(".lightgreen").click(function() {
+    $("body").addClass("lightgreen-background");
+    $(this).hide();
+    $(".blue").show();
+  });
 
+  $(".blue").click(function() {
+    $("body").addClass("blue-background");
+    $(this).hide();
+    $(".reset").show();
+  });
+
+
+  $(".reset").click(function() {
+    $("body").removeClass();
+    $(this).hide();
+    $(".green").show();
   });
 
 });
